c537739a51ea6b0306c97f9c9bcfeed5cf21aa25
Art <77998829+artdlc20973@users.noreply.github.com>
2022-02-18 11:06:50 -0700

    event handlers used with input forms
    event handlers used with input forms REACT

---
+++ build/precache-manifest.5664005e226080a278eaac9599a6054e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfbfebea82dde21d9ee29cac011e18bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41e8a95462b1230d3eb2",
+    "url": "/static/js/2.51196377.chunk.js"
+  },
+  {
+    "revision": "f9b3d370f10be9634900",
+    "url": "/static/js/main.fe57455a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
